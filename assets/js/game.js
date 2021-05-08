4d927c62534512f755b96363568c4dae8ff26af1
Reese B <theresa.byrket@gmail.com>
2021-05-08 14:57:54 -0500

    update console.log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+//Note the lack of qutations marks around playerName
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10 + 10);
+//What is this
+console.log("Your robot's name is" + playerName);
+function fight () {
+    window.alert("The fight has begun!");
+}
+
+//fight ();
